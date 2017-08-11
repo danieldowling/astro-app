b9bde1a6a4fecc3c6685bbcf0b08ac0e7420c712
Daniel Dowling <ddowling30@gmail.com>
2017-08-11 02:21:20 +0100

    update to server.js for heroku deploy

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ var http = require('http');
 var https = require('https');
 var bodyParser = require('body-parser');
 
+app.use(express.static(__dirname + '/dist'));
+
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
